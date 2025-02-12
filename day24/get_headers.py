b07c801ff1eceaac2e669683a32ce5d253cf14d8
Heitor Paulo <heitorino1@gmail.com>
2025-02-12 20:30:35 +0100

    improved code

--- day24/get_headers.py
+++ day24/get_headers.py
@@ -4,23 +4,41 @@ from datetime import datetime
 
 def generate_requests(url_list):
     if url_list:
-        print(url_list)
+        try:
+            write_to_file(url_list)
+            print("Requests completed successfully.")
+        except Exception as e:
+            print(f"Something went wrong", e)
+            exit(1)
     else:
-        with open("urls.txt") as files:
-            content = files.read()
-            lines = content.splitlines()
-            # files.write(f"{'='*50} \n")
-
-        with open("url_response", "w") as response:
-            time_of_request = f"{datetime.now()}"
-            for line in lines:
-                regex_match = re.match(r"https?://\S+", line)
-                if not regex_match:
-                    continue
-                head = requests.get(line)
-                response.write(f"{line} - {time_of_request}\n")
-                response.write(f"{'-' * 50}\n")
-                response.write(f"Headers: {head.headers}\nStatus-Code: {head.status_code}\n")
-                response.write(f"\n")
+        try:
+            lines = open_file("urls.txt")
+            write_to_file(lines)
+            print(f" All requests completed successfully")
+        except Exception as e:
+            print(f"Something went wrong", e)
+            exit(1)
 
 
+
+
+# File functions
+def open_file(file_path):
+    with open(file_path) as files:
+        content = files.read()
+        lines = content.splitlines()
+        return lines
+        # files.write(f"{'='*50} \n")
+
+def write_to_file(lines_from_text):
+    with open("url_response", "w") as response:
+        time_of_request = f"{datetime.now()}"
+        for line in lines_from_text:
+            regex_match = re.match(r"https?://\S+", line)
+            if not regex_match:
+                continue
+            head = requests.get(line)
+            response.write(f"{line} - {time_of_request}\n")
+            response.write(f"{'-' * 50}\n")
+            response.write(f"Headers: {head.headers}\nStatus-Code: {head.status_code}\n")
+            response.write(f"\n")
--- day24/main.py
+++ day24/main.py
@@ -9,10 +9,14 @@ while True:
     url_list_from_user.append(user_input)
     more_url = input("Are you done? [Y/N]:")
     if more_url.lower() == "y":
-        user_input = input("Type the url you want to request: ")
-    else:
         print("Ok, We are Working...")
-        generate_requests(url)
+        generate_requests(url_list_from_user)
+        break
+    else:
+        user_input = input("Type the url you want to request: ")
+        url_list_from_user.append(user_input)
+
+
 
 
 
